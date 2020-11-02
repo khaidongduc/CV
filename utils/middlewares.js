31496cfbb5b53ebdd7cd7d143e0cfa72592ba308
khaidongduc <khaidongduc@gmail.com>
2020-11-02 15:05:12 +0700

    add required login flash and fix flash button

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,6 +1,7 @@
 module.exports.ensureLoggedIn = function (req, res, next) {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
+        req.flash("error", "Need to login");
         res.redirect('/admin/login');
         return;
     }
